6fb143ee61c3cdc154b577d77e6da105228d887a
Nikhil Vijayan <nkhilv@gmail.com>
2018-11-02 11:28:12 +0000

    changed mute to play/pause

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var audio = document.getElementById('audio');
 var toggle = document.getElementById('toggleaudio');
+let isPlaying = false;
+
+audio.onplaying = ()=> isPlaying = true;
+audio.onpause = () => isPlaying = false;
 
 document.getElementById('toggleaudio').addEventListener('click', function (e)
 {
     e = e || window.event;
-    audio.muted = !audio.muted;
-    toggle.classList.toggle('unmute');
+    // audio.muted = !audio.muted;
+    // toggle.classList.toggle('unmute');
+    
+    if (isPlaying){
+        audio.pause();
+    } else {
+        audio.play();
+    }
+
+    if (audio.paused) {
+        toggle.innerHTML = "<img src=\"img/play.png\">";
+    } else {
+        toggle.innerHTML = "<img src=\"img/pause.png\">";
+    }
     e.preventDefault();
 }, true);
